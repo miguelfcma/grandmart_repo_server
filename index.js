d9a260482f2cb505c943a8cf484b0e8d49a297da
miguelfcma <107379032+miguelfcma@users.noreply.github.com>
2023-06-27 18:27:06 -0600

    cambios aws 1.0

--- index.js
+++ index.js
@@ -10,46 +10,9 @@ async function main() {
       console.log(`Servidor escuchando en el puerto ${PORT}`);
     });
   } catch (error) {
-    console.log("Conexión a la base de datos incompleta ", error);
+    console.log(`Conexión a la base de datos incompleta `, error);
   }
 }
 
 main();
 
-/* 
-import "./models/productosModel/ProductoModel.js";
-import  "./models/productosModel/FavoritoProductoModel.js";
-import  "./models/productosModel/ImagenesProductoModel.js";
-import  "./models/productosModel/ReviewsProductosModel.js";
-import  "./models/productosModel/PreguntasProductoModel.js";
-import "./models/productosModel/CarritoProductosModel.js";
-import  "./models/ordenesModel/EstadoPedidoModel.js";
-import  "./models/ordenesModel/EstadoPagoModel.js";
-import  "./models/ordenesModel/OrdenModel.js";
-import "./models/ordenesModel/ProductosOrdenModel.js";
-
-
-
-
-import  "./models/usuariosModel/UsuarioModel.js";
-import  "./models/usuariosModel/ImagenesUsuariosModel.js";
-import  "./models/usuariosModel/DomicilioUsuarioModel.js";
-
-import  "./models/serviciosModel/ImagenesServiciosModel.js";
-import  "./models/serviciosModel/PreguntasServiciosModel.js";
-import "./models/serviciosModel/ReviewsServiciosModel.js";
-import  "./models/serviciosModel/ServicioModel.js";
-import  "./models/serviciosModel/UbicacionServiciosModel.js";
-
-
-
-import "./models/buzonModel/ImagenBuzonModel.js";
-import  "./models/blogModel/PublicacionesBlogModel.js";
-import "./models/blogModel/ComentariosBlogModel.js";
-import  "./models/blogModel/ImagenesBlogModel.js";
-
-import "./models/serviciosModel/ServicioDatosContactoModel.js";
-import "./models/categoriasModel/CategoriaModel.js";
-
-import "./models/foreignKeys/foreignKeys.js"
-*/
